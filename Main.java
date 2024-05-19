f1f64c5c89eca0315ff4925db25fb970eeec5a30
MudassarAkram <153826328+MudassarAkr@users.noreply.github.com>
2024-05-19 19:02:11 +0500

    Main.java

--- Main.java
+++ Main.java
@@ -6,6 +6,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        new GameFrame();
+        new GameFrame(); // Create and display the game window
     }
 }
