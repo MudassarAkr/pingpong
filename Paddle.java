46da05f90c188dfed29a1fc486904c50918dbaca
MudassarAkram <153826328+MudassarAkr@users.noreply.github.com>
2024-06-10 02:50:59 +0500

    Paddle.java

--- Paddle.java
+++ Paddle.java
@@ -1,17 +1,18 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.util.*;
-import javax.swing.*;
+
 
 public class Paddle extends Rectangle {
 
     int id;
     int yVelocity;
     int speed = 10;
+    
 
-    Paddle(int x,int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, int id){
-        super(x,y,PADDLE_WIDTH,PADDLE_HEIGHT);
+    Paddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, int id) {
+        super(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
         this.id = id;
+        
     }
 
     public void keyPressed(KeyEvent e){
@@ -74,12 +75,13 @@ public class Paddle extends Rectangle {
         y = y+yVelocity;
     }
 
-    public void draw(Graphics g){
-        if (id==1)
+    public void draw(Graphics g) {
+        if (id == 1) {
             g.setColor(Color.blue);
-        else
+        } else {
             g.setColor(Color.red);
-
+        }
         g.fillRect(x,y,width,height);
     }
+    
 }
